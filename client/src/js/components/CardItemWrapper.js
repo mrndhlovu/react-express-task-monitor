a0d818af88840d89c72bf2f78a30c77f0da43f17
Mduduzi <ndhlovu@pop-os.localdomain>
2020-01-28 14:41:04 +0000

    add missing key

--- client/src/js/components/CardItemWrapper.js
+++ client/src/js/components/CardItemWrapper.js
@@ -3,6 +3,6 @@ import React from "react";
 import CardItem from "./CardItem";
 
 const CardItemWrapper = ({ cards, ...rest }) =>
-  cards.map(card => <CardItem key={card.id} card={card} {...rest} />);
+  cards.map(card => <CardItem key={card.position} card={card} {...rest} />);
 
 export default CardItemWrapper;
