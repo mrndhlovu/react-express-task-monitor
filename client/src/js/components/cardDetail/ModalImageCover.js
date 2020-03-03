2a4a2cdd6534d86bff193dad43475b513bd77c91
Mduduzi <ndhlovu@pop-os.localdomain>
2020-03-03 00:38:55 +0000

    remove unsed props

--- client/src/js/components/cardDetail/ModalImageCover.js
+++ client/src/js/components/cardDetail/ModalImageCover.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 
 import { Button, Modal } from "semantic-ui-react";
 import CardDetailSegment from "../sharedComponents/CardDetailSegment";
-import { getAverageRGB } from "../../utils/appUtils";
 
 const ButtonWrapper = styled.div`
   position: absolute;
