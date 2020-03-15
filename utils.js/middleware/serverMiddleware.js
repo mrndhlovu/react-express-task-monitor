c8c36535ca970ca621bdc87eb5aa7a963744a6dd
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-03-15 22:49:48 +0000

    Fix server path issue

--- utils.js/middleware/serverMiddleware.js
+++ utils.js/middleware/serverMiddleware.js
@@ -13,7 +13,7 @@ serverConfig = server => {
       credentials: true
     })
   );
-  server.use(express.static(path.join(__dirname, "client/build")));
+  server.use(express.static(path.join(__dirname, "client/public/index.html")));
   server.use((err, req, res, next) => res.json(err));
 };
 
