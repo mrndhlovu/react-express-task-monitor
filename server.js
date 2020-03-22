1a48516a7070a9381c7d882a625f0624e142844d
Mduduzi <ndhlovu@pop-os.localdomain>
2020-03-22 10:21:52 +0000

    resolve client path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { routesConfig } = require("./utils.js/middleware/routesMiddleware");
 const { serverConfig } = require("./utils.js/middleware/serverMiddleware");
 const { socketConfig } = require("./utils.js/middleware/socketIoMiddleware");
 const express = require("express");
+const path = require("path");
 const log = require("./utils.js/console-alert");
 const http = require("http");
 require("./utils.js/mongooseDB");
@@ -13,5 +14,9 @@ const server = http.createServer(app);
 serverConfig(app);
 socketConfig(server);
 routesConfig(app);
+app.use(express.static("/client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 server.listen(PORT, () => log.success(`Server running on port ${PORT}`));
