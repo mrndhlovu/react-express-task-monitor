ea4414ca9e1964b09ee18300948050de4769e035
Mduduzi <mrndhlovuie@gmail.com>
2020-04-18 23:54:24 +0100

    fix memory leak from protected route

--- client/src/js/Routes.js
+++ client/src/js/Routes.js
@@ -1,6 +1,6 @@
 "use es6";
 
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import HomePageContainer from "./containers/HomePageContainer";
@@ -9,24 +9,33 @@ import SignupContainer from "./containers/SignupContainer";
 import LoginContainer from "./containers/LoginContainer";
 import ErrorPage from "./components/ErrorPage";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { MainContext } from "./utils/contextUtils";
 
 export default function Routes() {
+  const { auth } = useContext(MainContext);
   return (
     <Switch>
-      <ProtectedRoute key="/" exact path="/" component={HomePageContainer} />
+      <ProtectedRoute
+        auth={auth}
+        key="/"
+        exact
+        path="/"
+        component={HomePageContainer}
+      />
       <ProtectedRoute
         key="boardDetail"
         path="/boards/id/:id"
         component={BoardContainer}
+        auth={auth}
       />
 
       <Route
         path="/login"
-        render={props => <LoginContainer key="login" {...props} />}
+        render={(props) => <LoginContainer key="login" {...props} />}
       />
       <Route
         path="/signup"
-        render={props => <SignupContainer key="signup" {...props} />}
+        render={(props) => <SignupContainer key="signup" {...props} />}
       />
       <Route path="*" component={ErrorPage} />
     </Switch>
