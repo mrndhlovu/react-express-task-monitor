483617442f1736344f7bdda819536fe60ea6e826
Mduduzi <mrndhlovuie@gmail.com>
2020-04-25 00:55:23 +0100

    add Dynamic imports at route level

--- client/js/Routes.js
+++ client/js/Routes.js
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import HomePageContainer from "./containers/HomePageContainer";
-import BoardContainer from "./containers/BoardContainer";
-import SignupContainer from "./containers/SignupContainer";
-import LoginContainer from "./containers/LoginContainer";
-import ErrorPage from "./components/ErrorPage";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
+import UILoadingSpinner from "./components/sharedComponents/UILoadingSpinner";
+
+class DynamicImport extends Component {
+  state = { component: null };
+
+  componentWillMount() {
+    this.props
+      .load()
+      .then((mod) => this.setState(() => ({ component: mod.default })));
+  }
+
+  render() {
+    return this.props.children(this.state.component);
+  }
+}
+
+const LoginContainer = (props) => (
+  <DynamicImport load={() => import("./containers/LoginContainer")}>
+    {(Component) =>
+      !Component ? <UILoadingSpinner /> : <Component {...props} />
+    }
+  </DynamicImport>
+);
+
+const ErrorPage = (props) => (
+  <DynamicImport load={() => import("./components/ErrorPage")}>
+    {(Component) =>
+      !Component ? <UILoadingSpinner /> : <Component {...props} />
+    }
+  </DynamicImport>
+);
+
+const HomePageContainer = (props) => (
+  <DynamicImport load={() => import("./containers/HomePageContainer")}>
+    {(Component) =>
+      !Component ? <UILoadingSpinner /> : <Component {...props} />
+    }
+  </DynamicImport>
+);
+
+const SignupContainer = (props) => (
+  <DynamicImport load={() => import("./containers/SignupContainer")}>
+    {(Component) =>
+      !Component ? <UILoadingSpinner /> : <Component {...props} />
+    }
+  </DynamicImport>
+);
+
+const ProtectedRoute = (props) => (
+  <DynamicImport load={() => import("./components/auth/ProtectedRoute")}>
+    {(Component) =>
+      !Component ? <UILoadingSpinner /> : <Component {...props} />
+    }
+  </DynamicImport>
+);
+
+const BoardContainer = (props) => (
+  <DynamicImport load={() => import("./containers/BoardContainer")}>
+    {(Component) =>
+      !Component ? <UILoadingSpinner /> : <Component {...props} />
+    }
+  </DynamicImport>
+);
 
 const Routes = () => {
   return (
