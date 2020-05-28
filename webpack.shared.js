06483cf2c8295f01d1b6c8a5e2b7ecf668d5634f
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-05-28 22:16:13 +0100

    Update webpack.shared.js

--- webpack.shared.js
+++ webpack.shared.js
@@ -1,9 +1,9 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { isDevelopment, environment } = require("./src/server/utils/config");
 const Dotenv = require("dotenv-webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 const webpack = require("webpack");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
