d6544f162af1eea4a913629bf7a6c4c891ba69c2
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-24 16:43:31 +0100

    edit passport redirect url

--- backend/utils/authUtils.js
+++ backend/utils/authUtils.js
@@ -3,8 +3,7 @@ const { ROOT_URL, isDevelopment } = require("./config");
 const getRedirectUrl = (req, token) => {
   const { email } = req.user;
 
-  return `https://trello-clone.ndhlovu.com/#/profile?token=${token}&email=${email}`;
-  // : `${ROOT_URL}/#/profile?token=${token}&email=${email}`;
+  return `${ROOT_URL}/#/profile?token=${token}&email=${email}`;
 };
 
 const generateAccessCookie = async (res, token) => {
