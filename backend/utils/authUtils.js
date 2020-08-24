865600b0cd19918694e610b614312dfbd2845ec4
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-08-24 15:16:12 +0100

    edit baseurl

--- backend/utils/authUtils.js
+++ backend/utils/authUtils.js
@@ -1,10 +1,11 @@
 const { ROOT_URL, isDevelopment } = require("./config");
 
-const RedirectUrl = (req, token) => {
+const getRedirectUrl = (req, token) => {
   const { email } = req.user;
 
-  return  `${ROOT_URL}/#/profile?token=${token}&email=${email}`
-   // : `http://172.31.32.194/#/profile?token=${token}&email=${email}`;
+  return isDevelopment
+    ? `${ROOT_URL}/#/profile?token=${token}&email=${email}`
+    : `/#/profile?token=${token}&email=${email}`;
 };
 
 const generateAccessCookie = async (res, token) => {
